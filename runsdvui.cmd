8ee1e776a6455f9aa4a3811265c4299394dbcd2f
Hypervisor <hyp3rvisor@protonmail.com>
2021-04-25 20:34:01 -0600

    This probably isn't important
    ohh noooo! muh folder structure was leaked!!!

--- runsdvui.cmd
+++
@@ -1,2 +0,0 @@
-cd /d "C:\Users\atom0\Desktop\GitHub\BSODConfigureDriver" &msbuild "BSODConfigureDriver.vcxproj" /t:sdvViewer /p:configuration="Debug" /p:platform="x64" /p:SolutionDir="C:\Users\atom0\Desktop\GitHub\BSODConfigureDriver" 
-exit %errorlevel% 
